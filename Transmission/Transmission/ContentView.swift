9a8d99742512ca1307f11fe45b907b988f274d05
Vitalii Yevtushenko <vitalii@rollncode.com>
2019-07-25 12:56:25 +0300

    Add new application for full rewrite of Transmission with Swift 5

---
+++ Transmission/Transmission/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Transmission
+//
+//  Created by Vitalii Yevtushenko on 25.07.2019.
+//  Copyright © 2019 Roll'n'Code. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
